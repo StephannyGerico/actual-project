c3bab8d06340ef6286d8d757db3f1145de4a8c43
StephannyGerico <stephannygerico@gmail.com>
2022-11-30 21:38:10 -0500

    first part project

---
+++ src/index.js
@@ -0,0 +1,79 @@
+let now = new Date();
+let h4 = document.querySelector("h4");
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+} else {
+  hours = `${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+} else {
+  minutes = `${minutes}`;
+}
+h4.innerHTML = `${day} ${hours}:${minutes}`;
+
+function searchCity(city) {
+  let apiKey = "de2c40e370d58e257faf07ba4ea95840";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+
+let searchForm = document.querySelector("#search");
+searchForm.addEventListener("submit", handleSubmit);
+
+function showTemperature(response) {
+  document.querySelector("#temp").innerHTML = Math.round(
+    response.data.main.temp
+  );
+
+  document.querySelector("#place").innerHTML = response.data.name;
+
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].description;
+
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+
+  document.querySelector("#feels").innerHTML = Math.round(
+    response.data.main.feels_like
+  );
+}
+
+function handlePosition(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let apiKey = "a95c2c6739994ba4903e007ee817e7d1";
+  let endPointe = "https://api.openweathermap.org/data/2.5/find";
+  let apiUrl = `${endPointe}?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(handlePosition);
+}
+
+let currentButton = document.querySelector("#button");
+currentButton.addEventListener("click", getCurrentLocation);
